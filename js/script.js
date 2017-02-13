42425abec742be3c285f9360ac80585fed2f22f3
Andrey <andrey-tm203@mail.ru>
2017-02-13 08:19:38 +0300

    Основную часть вроде сделал, остался слайдер, почтовая рамка, ну и может по мелочи еще что-то. На следующих выходных, надеюсь, сдам

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.feedback-btn').addEventListener('click', function(event) {
+    showFeedbackForm();
+    hidePlaceholders();
+});
+
+function showFeedbackForm() {
+    document.querySelector('.map-contacts-btn').addEventListener('click', function(event) {
         event.preventDefault();
         document.querySelector('.feedback-form').style.display = 'flex';
     });
+    closeFeedbackForm();
+}
+
+function closeFeedbackForm() {
     document.querySelector('.feedback-form-close').addEventListener('click', function(event) {
         event.preventDefault();
         document.querySelector('.feedback-form').style.display = 'none';
     });
-});
+}
+
+function hidePlaceholders() {
+    var inputs = document.querySelectorAll('.input-cover input');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('focus', function() {
+            hidePlaceholder(this);
+        });
+    }
+    document.querySelector('.text-area-cover textarea').addEventListener('focus', function() {
+        hidePlaceholder(this)
+    });
+}
+
+function hidePlaceholder(input) {
+    var str = input.getAttribute('placeholder');
+    input.removeAttribute('placeholder');
+    input.addEventListener('blur', function() {
+        this.setAttribute('placeholder', str);
+    });
+}
